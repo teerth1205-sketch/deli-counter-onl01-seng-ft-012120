5cc265be280fcfe061ac1a2cae4e400de06aba01
Teerth Patel <teerth1205@gmail.com>
2020-01-23 01:20:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,9 +26,10 @@ def take_a_number(katz_deli, name)
  end
  
  def now_serving(katz_deli)
-   if katz_deli == []
+   if katz_deli.length > 0 
      puts "There is nobody waiting to be served!".
-   else 
+   else
+      puts "There is nobody waiting to be served!".
      puts katz_deli[1] 
      katz_deli.shift
    end 
