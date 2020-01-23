deb6e22d099e7bcdff9174c00d75d3cf0a7874b5
Teerth Patel <teerth1205@gmail.com>
2020-01-23 00:29:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,12 @@
 def line(katz_deli)
   if katz_deli.length == 0
   puts "The line is currently empty."
-else
-  katz_deli.each do |name|
+  else
+    katz_deli.each do |name|
     count = 1
     "The line is currently: #{count}. #{name}"
     
     count += 1
   end
+end
 end
